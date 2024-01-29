c1e5c6ff9279ce0bfe290af5dc9c5d55ee5b0bb5
dev-ashanaur <devashanaur009@gmail.com>
2024-01-29 21:21:03 +0600

    [add] faculty update

--- src/app/modules/faculty/faculty.service.ts
+++ src/app/modules/faculty/faculty.service.ts
@@ -1,83 +1,102 @@
-import { Request } from "express";
-import { IGenericResponse } from "../../../interfaces/common";
-import { AuthService, CoreService } from "../../../shared/axios";
+import { Request } from 'express';
+import { IGenericResponse } from '../../../interfaces/common';
+import { AuthService, CoreService } from '../../../shared/axios';
 
 const getAllFromDB = async (req: Request): Promise<IGenericResponse> => {
-    const response: IGenericResponse = await CoreService.get('/faculties', {
-        params: req.query,
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
+  const response: IGenericResponse = await CoreService.get('/faculties', {
+    params: req.query,
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
 };
 
 const getMyCourses = async (req: Request): Promise<IGenericResponse> => {
-    const response: IGenericResponse = await CoreService.get('/faculties/my-courses', {
-        params: req.query,
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
+  const response: IGenericResponse = await CoreService.get('/faculties/my-courses', {
+    params: req.query,
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
 };
 
 const getMyCourseStudents = async (req: Request): Promise<IGenericResponse> => {
-    const response: IGenericResponse = await CoreService.get('/faculties/my-course-students', {
-        params: req.query,
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
+  const response: IGenericResponse = await CoreService.get('/faculties/my-course-students', {
+    params: req.query,
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
 };
 
 const getByIdFromDB = async (req: Request): Promise<IGenericResponse> => {
-    const { id } = req.params;
-    const response: IGenericResponse = await CoreService.get(`/faculties/${id}`, {
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
+  const { id } = req.params;
+  const response: IGenericResponse = await CoreService.get(`/faculties/${id}`, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
 };
 
 const getFacultyProfile = async (req: Request): Promise<IGenericResponse> => {
-    const { id } = req.params;
-    const response: IGenericResponse = await AuthService.get(`/faculties/${id}`, {
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
+  const { id } = req.params;
+  const response: IGenericResponse = await AuthService.get(`/faculties/${id}`, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
 };
 
 const updateOneInDB = async (req: Request): Promise<IGenericResponse> => {
-    const { id } = req.params;
-    const response: IGenericResponse = await AuthService.patch(`/faculties/${id}`, req.body, {
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
+  const { id } = req.params;
+
+  const { academicDepartment, academicFaculty } = req.body;
+
+  const academicDepartmentResponse = await AuthService.get(
+    `/academic-departments?syncId=${academicDepartment}`
+  );
+
+  if (academicDepartmentResponse.data && Array.isArray(academicDepartmentResponse.data)) {
+    req.body.academicDepartment = academicDepartmentResponse.data[0].id;
+  }
+
+  const academicFacultyResponse = await AuthService.get(
+    `/academic-faculties?syncId=${academicFaculty}`
+  );
+
+  if (academicFacultyResponse.data && Array.isArray(academicFacultyResponse.data)) {
+    req.body.academicFaculty = academicFacultyResponse.data[0].id;
+  }
+
+  const response: IGenericResponse = await AuthService.patch(`/faculties/${id}`, req.body, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
 };
 
 const deleteByIdFromDB = async (req: Request): Promise<IGenericResponse> => {
-    const { id } = req.params;
-    const response: IGenericResponse = await AuthService.delete(`/faculties/${id}`, {
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
+  const { id } = req.params;
+  const response: IGenericResponse = await AuthService.delete(`/faculties/${id}`, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
 };
 
 export const FacultyService = {
-    updateOneInDB,
-    getAllFromDB,
-    getByIdFromDB,
-    getMyCourses,
-    getFacultyProfile,
-    getMyCourseStudents,
-    deleteByIdFromDB
-}
+  updateOneInDB,
+  getAllFromDB,
+  getByIdFromDB,
+  getMyCourses,
+  getFacultyProfile,
+  getMyCourseStudents,
+  deleteByIdFromDB
+};
